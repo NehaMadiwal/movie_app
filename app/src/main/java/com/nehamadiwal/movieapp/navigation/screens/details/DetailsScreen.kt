539f7ec988d55fbac0c7a86c36fae312883120cd
Neha Madiwal <nehamadiwal@gmail.com>
2024-04-17 16:18:07 +0530

    added MovieRow on details screen and show image gallery using LazyRow

--- app/src/main/java/com/nehamadiwal/movieapp/navigation/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/nehamadiwal/movieapp/navigation/screens/details/DetailsScreen.kt
@@ -2,11 +2,20 @@ package com.nehamadiwal.movieapp.navigation.screens.details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,10 +33,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.nehamadiwal.movieapp.model.Movie
+import com.nehamadiwal.movieapp.model.getMovies
+import com.nehamadiwal.movieapp.widget.MovieRow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailsScreen(navController: NavController, movieData: String?) {
+fun DetailsScreen(navController: NavController, movieId: String?) {
+
+    val newMovieList = getMovies().filter { it.id == movieId }
 
     Scaffold(
         topBar = {
@@ -60,14 +75,34 @@ fun DetailsScreen(navController: NavController, movieData: String?) {
 
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
+            ) {
+                MovieRow(movie = newMovieList.first())
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                HorizontalScrollableImageView(newMovieList)
+            }
+        }
+    }
+}
+
+@Composable
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(newMovieList.first().images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp)
+                    .wrapContentHeight(Alignment.CenterVertically),
+                elevation = CardDefaults.cardElevation(5.dp)
             ) {
 
-                Text(
-                    text = movieData.toString(),
-                    style = MaterialTheme.typography.headlineSmall
-                )
+                AsyncImage(model = image, contentDescription = "Movie image")
+
             }
+
         }
     }
 }
